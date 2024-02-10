577aa0e63b839aaf9103f1d909529183a07fd4cc
Awwab <mawwabkhank2006@gmail.com>
2024-02-10 12:40:07 +0500

    search query and response from spotify

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import requests
 import spotipy
 import os
 from spotipy.oauth2 import SpotifyOAuth
+from pprint import pprint
 
 SPOTIPY_CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
 SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
 scope = "playlist-modify-private"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+user_id = sp.current_user()['id']
+date_to_travel = input('Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ')
+response = requests.get(f'https://www.billboard.com/charts/hot-100/{date_to_travel}/')
 
-# date_to_travel = input('Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ')
-# response = requests.get(f'https://www.billboard.com/charts/hot-100/{date_to_travel}/')
-
-# soup = BeautifulSoup(response.text, 'html.parser')
-# titles= [item.text.strip() for item in soup.select("li ul li h3")]
-# print(titles)
+soup = BeautifulSoup(response.text, 'html.parser')
+titles= [item.text.strip() for item in soup.select("li ul li h3")]
+results = sp.search(q=f"track: {titles[0]} year: {date_to_travel.split('-')[0]}", limit=1)
+pprint(results)
