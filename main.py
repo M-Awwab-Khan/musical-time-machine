8e4a729a1c790aed976776b16e90437f3eb9ef2b
Awwab <mawwabkhank2006@gmail.com>
2024-02-10 11:14:33 +0500

    stripped text to remove \n

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ date_to_travel = input('Which year do you want to travel to? Type the date in th
 response = requests.get(f'https://www.billboard.com/charts/hot-100/{date_to_travel}/')
 
 soup = BeautifulSoup(response.text, 'html.parser')
-titles= [item.text for item in soup.select("li ul li h3")]
+titles= [item.text.strip() for item in soup.select("li ul li h3")]
 print(titles)
