90f6e380c9d9ae2c345b6315214029ab5dc96f57
Awwab <mawwabkhank2006@gmail.com>
2024-02-10 11:22:49 +0500

    spotify api constants

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 
-
+SPOTIFY_CLIENT_ID = '93dbb833534a4706a75328ad309eb340'
+SPOTIFY_CLIENT_SECRET = 'aad35ccae6c248dbbf5a087699d04a36'
 date_to_travel = input('Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ')
 response = requests.get(f'https://www.billboard.com/charts/hot-100/{date_to_travel}/')
 
