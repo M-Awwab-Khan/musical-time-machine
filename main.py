c03cc47ccf29ed6f393493655a80b81bdc2bcb96
Awwab <mawwabkhank2006@gmail.com>
2024-02-10 11:39:59 +0500

    trying to setup spotify authentication

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
+import spotipy
+import os
+from spotipy.oauth2 import SpotifyOAuth
 
-SPOTIFY_CLIENT_ID = '93dbb833534a4706a75328ad309eb340'
-SPOTIFY_CLIENT_SECRET = 'aad35ccae6c248dbbf5a087699d04a36'
-date_to_travel = input('Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ')
-response = requests.get(f'https://www.billboard.com/charts/hot-100/{date_to_travel}/')
+SPOTIPY_CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
+SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
+scope = "playlist-modify-private"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-soup = BeautifulSoup(response.text, 'html.parser')
-titles= [item.text.strip() for item in soup.select("li ul li h3")]
-print(titles)
+# date_to_travel = input('Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ')
+# response = requests.get(f'https://www.billboard.com/charts/hot-100/{date_to_travel}/')
+
+# soup = BeautifulSoup(response.text, 'html.parser')
+# titles= [item.text.strip() for item in soup.select("li ul li h3")]
+# print(titles)
