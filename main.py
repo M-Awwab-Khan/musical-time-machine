6039c09ee0b46b65fafcf600625a0ad457153de0
Awwab <mawwabkhank2006@gmail.com>
2024-02-10 11:13:31 +0500

    got hold of songs from website

---
+++ main.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+date_to_travel = input('Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ')
+response = requests.get(f'https://www.billboard.com/charts/hot-100/{date_to_travel}/')
+
+soup = BeautifulSoup(response.text, 'html.parser')
+titles= [item.text for item in soup.select("li ul li h3")]
+print(titles)
